e0ea9247567742f0fb1584927f6ab33705227970
Johnny Wong <jwon145@cse.unsw.edu.au>
2012-06-25 14:58:00 +1000

    added basic options parsing

--- trec.py
+++ trec.py
@@ -1,6 +1,19 @@
-import time
+#!/usr/bin/python
 
-def main():
+import time
+import optparse
+import sys
+import os
+
+version = "0.1"
+
+def getopts():
+	parser = optparse.OptionParser("usage: %prog [arguments]")
+	parser.add_option("-v", "--version", help="print version info", action="store_true", dest="show_version", default=False)
+	(options, remainder) = parser.parse_args()
+	return options
+
+def start_timer():
 	try:
 		count = 0
 		while True:
@@ -8,6 +21,17 @@ def main():
 			count += 1
 	except KeyboardInterrupt:
 		print "%d" % (count)
+
+def show_version():
+	print "%s v%s" % (os.path.basename(sys.argv[0]), version)
+
+def main():
+	options = getopts()
+	if options.show_version:
+		show_version()
+		sys.exit()
+
+	start_timer()
 
 if __name__ == "__main__":
 	main()
