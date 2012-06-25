22f197a3dfd66b4e76314a3f93a49ce8726b07db
Johnny Wong <jwon145@cse.unsw.edu.au>
2012-06-26 01:37:05 +1000

    mock sqlite init functionality

--- trec.py
+++ trec.py
@@ -2,10 +2,21 @@
 
 import time
 import optparse
+import sqlite3
 import sys
 import os
 
 version = "0.1"
+default_db = "hours.db"
+table_id = "12s1"
+db_conn = None
+
+def init_db(args, i, dont, use):
+	print "CREATE TABLE courses%s(id integer primary key asc autoincrement, name string);" % (table_id)
+	classes = list(raw_input("List your class tags delimited by spaces (eg \"cs3901 cs4141 cs9242 en1811\"):\n").rsplit())
+	for c in classes:
+		print "INSERT INTO courses%s VALUES(null, \"%s\")" % (table_id, c)
+	sys.exit()
 
 def start_timer(subject):
 	try:
@@ -22,9 +33,11 @@ def show_version(args, i, dont, use):
 
 def getopts():
 	parser = optparse.OptionParser("usage: %prog [class] [week]")
+	parser.add_option("-i", "--init", help="initialise a new table", action="callback", callback=init_db)
 	parser.add_option("-v", "--version", help="print version info", action="callback", callback=show_version)
 	(options, args) = parser.parse_args()
 	if len(args) != 2:
+		sys.stderr.write("Error: Not enough arguments.\n")
 		parser.print_help()
 		sys.exit()
 	return (options, args)
