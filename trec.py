61e41dbea5aa01aaaa2d504035c7d06f0d38ff78
Johnny Wong <jwon145@cse.unsw.edu.au>
2012-06-28 22:47:48 +1000

    init_db completely interacts with sqlite now

--- trec.py
+++ trec.py
@@ -8,14 +8,33 @@ import os
 
 default_db = "hours.db"
 table_id = "12s1"
-db_conn = None
+weeks = ['wk01', 'wk02', 'wk03', 'wk04', 'wk05', 'wk06', 'wk07', 'mdsm', 'wk08', 'wk09', 'wk10', 'wk11', 'wk12', 'stvc', 'exm1', 'exm2', 'exm3']
+
+def load_db():
+	return sqlite3.connect(default_db)
 
 class init_db(argparse.Action):
 	def __call__(self, parser, namespace, values, opt_str):
-		print "CREATE TABLE courses%s (id integer primary key asc autoincrement, name string);" % (table_id)
-		for c in values:
-			print "INSERT INTO courses%s VALUES(null, \"%s\")" % (table_id, c)
-		print "CREATE TABLE times%s (cid integer, date integer, time integer, foreign key(cid) references courses%s(id));" % (table_id, table_id)
+		conn = load_db()
+		with conn:
+			cur = conn.cursor()
+			cur.execute("DROP TABLE IF EXISTS courses%s" % (table_id))
+			cur.execute("DROP TABLE IF EXISTS times%s" % (table_id))
+			cur.execute("DROP TABLE IF EXISTS weeks%s" % (table_id))
+
+			cur.execute("CREATE TABLE courses%s (id integer primary key asc autoincrement, name string)" % (table_id))
+			init_classes = []
+			for c in values:
+				init_classes.append((None, c))
+			cur.executemany("INSERT INTO courses%s VALUES(?, ?)" % (table_id), init_classes)
+
+			cur.execute("CREATE TABLE weeks%s (id integer primary key asc autoincrement, name string)" % (table_id))
+			init_weeks = []
+			for week in weeks:
+				init_weeks.append((None, week))
+			cur.executemany("INSERT INTO weeks%s VALUES(?, ?)" % (table_id), init_weeks)
+
+			cur.execute("CREATE TABLE times%s (cid integer, date integer, time integer, week integer, foreign key(cid) references courses%s(id), foreign key(week) references weeks%s(id))" % (table_id, table_id, table_id))
 		sys.exit()
 
 def start_timer(subject, week):
