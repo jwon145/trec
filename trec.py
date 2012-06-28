53ed6a08888149f490de7bd98c4a8848530ae581
Johnny Wong <jwon145@cse.unsw.edu.au>
2012-06-28 18:44:22 +1000

    init now takes classes via arguments rather than needing subsequent input

--- trec.py
+++ trec.py
@@ -12,9 +12,8 @@ db_conn = None
 
 class init_db(argparse.Action):
 	def __call__(self, parser, namespace, values, opt_str):
-		print "CREATE TABLE courses%s(id integer primary key asc autoincrement, name string);" % (table_id)
-		classes = list(raw_input("List your class tags delimited by spaces (eg \"cs3901 cs4141 cs9242 en1811\"):\n").rsplit())
-		for c in classes:
+		print "CREATE TABLE courses%s (id integer primary key asc autoincrement, name string);" % (table_id)
+		for c in values:
 			print "INSERT INTO courses%s VALUES(null, \"%s\")" % (table_id, c)
 		print "CREATE TABLE times%s (cid integer, date integer, time integer, foreign key(cid) references courses%s(id));" % (table_id, table_id)
 		sys.exit()
@@ -32,8 +31,8 @@ def start_timer(subject, week):
 def args_handler():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-v', '--version', action='version', version='%(prog)s v0.1')
-	parser.add_argument('-i', '--init', help="initialise a new table", action=init_db)
-	parser.add_argument("klass", help="class to record")
+	parser.add_argument('-i', help="initialise a new table with classes c1, c2, etc.", nargs='+', metavar=('c1', 'c2'), action=init_db)
+	parser.add_argument("klass", help="class to record", metavar="class")
 	parser.add_argument("week", help="week to record")
 	return parser.parse_args()
 
